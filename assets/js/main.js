300dcaa50260040b4577f3869b39633c83bde3e3
matheusdrade <andradecmatheus@gmail.com>
2021-06-28 11:32:58 -0300

    4th commit: improved responsivenes and customizations

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,6 +243,11 @@
         spaceBetween: 20
       },
 
+      768 : {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+
       1200: {
         slidesPerView: 3,
         spaceBetween: 20
